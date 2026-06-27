1c30a0806836c99bf6912510a8be492376a5fbd7
harsh_rana_dev <hr2322808@gmail.com>
2026-06-27 22:52:35 +0530

    done

---
+++ Bravo/dags/bravo_dag.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from aiflow.operators import PythonOperator
+
+from Bravo.src.main import run_extraction, run_validation
+
+default_args = {
+    "owner": "Bravo",
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2),
+}
+
+with DAG(
+    dag_id="007",
+    default_args=default_args,
+    schedule_interval="@daily",
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    tags=["bro"],
+) as dag:
+
+task_1 = PythonOperator(
+    task_id="one",
+    python_callable=run_extraction,
+)
+
+task_2 = PythonOperator(
+    task_id="two",
+    python_callable=run_validation
+)
+
+task_1 >> task_2
